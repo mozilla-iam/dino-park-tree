8a1584e9ba3b7f13184772491ee4dc6d1bfda8d2
Florian Merz <flomerz@gmail.com>
2018-10-08 16:05:05 +0200

    More storage tests.

--- test/storageTest.js
+++ test/storageTest.js
@@ -11,17 +11,78 @@ import { TEST_CONFIG } from "./configs";
 import { generateProfile } from "./helpers";
 import Storage from "../lib/storage";
 
-function makeEs() {
+function makeHits(num) {
+  return Array.apply(null, { length: num })
+    .map(Number.call, Number)
+    .map(i => {
+      return { _source: i };
+    });
+}
+
+function makeEs(how) {
   class Es {
-    constructor() {}
+    constructor() {
+      this.indices = {};
+      this.indices.exists = async () => how.exists;
+      this.indices.refresh = async () => how.refresh;
+      this.scrollId = 0;
+    }
+    async search({ scroll }) {
+      this.scrollId = scroll && 0;
+      return {
+        hits: {
+          total: how.total,
+          hits: how.searchHits,
+          _scroll_id: scroll && 1
+        }
+      };
+    }
+    async scroll() {
+      return {
+        hits: {
+          total: how.total,
+          hits: how.scrollHits[this.scrollId++] || [],
+          _scroll_id: 1
+        }
+      };
+    }
+    async delete() {}
   }
   return Es;
 }
 
 describe("constructor", () => {
-  it("constructor success", () => {
+  it("constructor success", async () => {
     const es = makeEs();
-    const storage = new Storage(TEST_CONFIG, es);
+    const storage = await new Storage(TEST_CONFIG, es).init();
     storage.should.exist;
+  });
+
+  it("get them all", async () => {
+    const hits = makeHits(150);
+    const es = makeEs({
+      exists: true,
+      total: 130,
+      searchHits: hits.splice(0, 50),
+      scrollHits: [hits.splice(0, 50), hits.splice(0, 30)]
+    });
+    const storage = new Storage(TEST_CONFIG, es);
+    const dinos = await storage.getDinos();
+    dinos.length.should.be.equal(130);
+  });
+
+  it("delete calls refresh", async () => {
+    const es = makeEs({ refresh: Promise.resolve(true) });
+    const storage = new Storage(TEST_CONFIG, es);
+    const dinos = await storage.deleteUser();
+    dinos.should.be.true;
+  });
+
+  it("update", async () => {
+    const profile = generateProfile(1, 0);
+    const es = makeEs({ refresh: Promise.resolve(true) });
+    const storage = new Storage(TEST_CONFIG, es);
+    const dinos = await storage.update(profile);
+    dinos.should.be.true;
   });
 });
